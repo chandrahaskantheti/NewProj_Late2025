9e1dd1dc9b810f0a9f412c2e99f3dbfba87f261a
Chandrahas Kantheti <chandra.kantheti@gmail.com>
2025-07-16 16:13:05 -0700

    Working Expense Tracker, very basic level

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ One might say a last attempt at the Software Engineering world and SDE/SWE world
 For me, this repo is my proof that I can code, debug, script, and prove that I don't lack coding skills, mastery and adaptability of new concepts,
 and am willing to turn a new leaf in my career. 
 # NewProj_Late2025
+
+Project 1: Expansion-Based ExpenseTracking Auto Script
--- expenseTracker.py
+++ expenseTracker.py
@@ -1,13 +1,34 @@
-file = 'ProjectsMidLate2025/expenses.csv'
+import os
+import csv
+from datetime import datetime
 
-def read_csv(file):
-  with open(file, 'r') as f:
-      readLines = f.read()
-      print(readLines)
+file = 'expenses.csv'
+
+
+def setup_csv(file):
+  if not os.path.exists(file):
+    with open(file, mode="w", newline='') as f:
+      writer = csv.writer(f)
+      if os.path.getsize(file) == 0:
+        writer.writerow(['Date', 'Category', 'Amount', 'Description'])
+      else:
+        writer = csv.writer(f)
     
+def view_expenses(file):
+  if os.path.exists(file):
+    with open(file, mode="r") as f:
+      reader = csv.reader(f)
+      next(reader)
+      for row in reader:
+        print(f"Date: {row[0]}, Category: {row[1]}, Amount: ${row[2]}, Description: {row[3]}")
+  else:
+    print("No expenses recorded yet.")
+
 def main():
   print("Expense Tracker")
-  read_csv(file)
+  setup_csv(file)
+  
+  view_expenses(file)
   
 if __name__ == "__main__":
   main()
