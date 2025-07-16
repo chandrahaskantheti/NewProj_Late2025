3db547776284e24593fb5296f816e15af5be14ee
Chandrahas Kantheti <chandra.kantheti@gmail.com>
2025-07-16 15:54:57 -0700

    Relearn CSV file reads: Next conversion json

--- expenseTracker.py
+++ expenseTracker.py
@@ -1,2 +1,13 @@
-file = open('expenses.csv', 'w')
-print(file) 
+file = 'ProjectsMidLate2025/expenses.csv'
+
+def read_csv(file):
+  with open(file, 'r') as f:
+      readLines = f.read()
+      print(readLines)
+    
+def main():
+  print("Expense Tracker")
+  read_csv(file)
+  
+if __name__ == "__main__":
+  main()
